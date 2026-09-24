37b4fed60c7d1a7d789b1d6dac528f3676aabf50
cyclonekillzyt <kendrickscyclone@gmail.com>
2026-09-24 12:01:09 +0000

    stop tracking .env

--- app.config.ts
+++ app.config.ts
@@ -5,9 +5,22 @@ export default {
     name: "school-management-app",
     slug: "school-management-app",
     version: "1.0.0",
+    ios: {
+      bundleIdentifier: "com.cycloneking.schoolmanagementapp",
+      infoPlist: {
+        ITSAppUsesNonExemptEncryption: false,
+      },
+    },
+    android: {
+      package: "com.cycloneking.schoolmanagementapp",
+      versionCode: 1,
+    },
     extra: {
       supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
       supabasePublishableKey: process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
+      eas: {
+        projectId: "73489df7-ee7d-4ca0-8f20-6c9772137794",
+      },
     },
     plugins: ["expo-sharing", "expo-splash-screen"],
   },
